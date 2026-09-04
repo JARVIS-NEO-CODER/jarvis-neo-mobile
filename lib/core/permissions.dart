26622e1af84d1bee47606e3c38f3047594994321
JARVIS-NEO-CODER <project.jarvis.neo@gmail.com>
2026-09-04 21:43:09 +0200

    feat: define granular capability permissions

---
+++ lib/core/permissions.dart
@@ -0,0 +1,5 @@
+class JarvisPermissions {
+  static const capabilities = <String>[
+    'camera','microphone','location','bluetooth','nfc','notifications','contacts','calendar','phone','messages','files','screen_context','remote_pc','smart_home','sensors',
+  ];
+}
